65fbe1cd4504a616149b03ac368315775b83e420
micboydoodle <mpschmidtlein@gmail.com>
2019-03-14 15:49:06 -0400

    3/14/19

---
+++ Assignments/lesson- 7/windchill.js
@@ -0,0 +1,7 @@
+var temp = parseInt(document.getElementById('currentTemp').innerHTML);
+var speed = parseInt(document.getElementById('windSpeed').innerHTML);
+
+var windChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+
+windChill = Math.round(windChill);
+document.getElementById('windChill').innerHTML = windChill;
